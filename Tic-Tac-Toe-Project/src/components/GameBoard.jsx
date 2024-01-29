7972be9e295607cb5acb0738762102462b1b9040
Rajat Kumar <rajatprajapati2016@gmail.com>
2024-01-29 22:14:40 +0530

    updated code to show active player

--- Tic-Tac-Toe-Project/src/components/GameBoard.jsx
+++ Tic-Tac-Toe-Project/src/components/GameBoard.jsx
@@ -1,17 +1,34 @@
-export default function GameBoard(){
-    let arr = [
-        [null,null,null],
-        [null,null,null],
-        [null,null,null],
-    ]
+import { useState } from "react";
+
+let initialArr = [
+    [null,null,null],
+    [null,null,null],
+    [null,null,null],
+]
+
+export default function GameBoard({currentPlayer,activeSymbol}){
+    
+    const [gameboard, setGameBoard] = useState(initialArr);
+
+    function handleClick(rowIndex, colIndex){
+        setGameBoard((previousGameBoard) =>{
+            const updatedGameBoard = [...previousGameBoard];
+            updatedGameBoard[rowIndex][colIndex] = activeSymbol;
+            return updatedGameBoard;
+        })
+
+        currentPlayer();
+    }
 
     return(
         <ol id="game-board">
-            {arr.map((row, rowIndex) => (
+            {gameboard.map((row, rowIndex) => (
             <li key={rowIndex}>
                 <ol>
                     {row.map((col,colIndex) => (
-                        <li key={colIndex}><button>{col}</button></li>
+                        <li key={colIndex}>
+                            <button onClick={()=> handleClick(rowIndex,colIndex)}>{col}</button>
+                        </li>
                     ))}
                 </ol>
             </li>)
--- Tic-Tac-Toe-Project/src/components/Player.jsx
+++ Tic-Tac-Toe-Project/src/components/Player.jsx
@@ -1,6 +1,6 @@
 import { useState } from "react";
 
-export default function Player({initialName,symbol}){
+export default function Player({initialName,symbol,isActive}){
     const [name, setName] = useState(initialName);
     const [isEdited,isEditing] = useState(false);
 
@@ -20,7 +20,7 @@ export default function Player({initialName,symbol}){
     }
 
     return (
-        <li>
+        <li className={isActive ? 'active' : undefined}>
             <span className="player">
               {player}
               <span className="player-symbol">{symbol}</span>
