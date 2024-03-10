6a52259c974e83c00ac65bb6fc62e5fdc80711bb
Rajat Kumar <rajatprajapati2016@gmail.com>
2024-03-10 16:40:58 +0530

    added new project management project

---
+++ Project-Management/src/App.jsx
@@ -0,0 +1,10 @@
+import CreateNewProject from "./components/CreateNewProject";
+function App() {
+  return (
+    <>
+      <CreateNewProject></CreateNewProject>
+    </>
+  );
+}
+
+export default App;
---
+++ Project-Management/src/components/CreateNewProject.jsx
@@ -0,0 +1,14 @@
+export default function CreateNewProject(){
+    return(
+        // <div className="bg-white p-8 rounded shadow-md max-w-md">
+        //     <img className="w-16 h-16 object-contain mx-auto" src="logo.png" alt="image not found"></img>
+        //     <h2 className="text-xl font-bold text-stone-700 my-4">No Project Selected</h2>
+        // </div>
+        <div className="mt-24 text-center w-2/3">
+            <img className="w-16 h-16 object-contain mx-auto" src="logo.png"  alt="img not found"/>
+            <h2 className="text-xl font-bold text-stone-700 my-4">No Project Selected</h2>
+            <p className="text-lg mb-8">Select a project or get started with a new one</p>
+            <button className="px-6 py-2 rounded-md bg-stone-800 text-stone-50 hover:bg-stone-950">Create new project</button>
+        </div>
+    )
+}
