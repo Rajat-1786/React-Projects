d31e2e15d2de340e874bcde32d342b73430487cc
Rajat Kumar <rajatprajapati2016@gmail.com>
2024-03-11 19:41:39 +0530

    updated Project information read form

--- Project-Management/src/components/SidePanel.jsx
+++ Project-Management/src/components/SidePanel.jsx
@@ -2,7 +2,7 @@ import { useState } from "react";
 
 
 export default function SidePanel({showProject}){
-    
+
     return(
         <>
         <aside className="w-1 px-8 py-16 bg-stone-900 text-stone-50 md:w-72 rounded-r-xl">
@@ -13,6 +13,11 @@ export default function SidePanel({showProject}){
                 <button onClick={showProject} className="px-4 py-2 text-xs md:text-base rounded-md bg-stone-700 text-stone-400 hover:bg-stone-600 hover:text-stone-100">
                     + Add Project
                 </button>
+            </div>
+            <div>
+                <ul>
+                    <li></li>
+                </ul>
             </div>
         </aside>
         </>
